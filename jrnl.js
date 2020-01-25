ce51d732f28ef08ea165ce2168c767c99a2dd2c2
bothzoli <bothzoli@gmail.com>
2020-01-25 16:09:13 +0100

    CLI + tag coloring

--- jrnl.js
+++ jrnl.js
@@ -1,1 +1,23 @@
-module.exports = (a, b) => a + b;
+const readline = require('readline');
+const entry = require('./src/entry');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '',
+});
+
+rl.write('Please write your jrnl entry!\n');
+rl.prompt();
+
+let jrnlEntry = '';
+
+rl.on('line', (line) => {
+  jrnlEntry += `${line}\n`;
+  rl.prompt();
+}).on('close', () => {
+  const newEntry = new entry.Entry(jrnlEntry);
+  console.log(newEntry.toString());
+  console.log(`tags: ${newEntry.tags}`);
+  process.exit(0);
+});
--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,14 @@
+const chalk = require('chalk');
+
 const tagCharacter = '~';
 
 const entryMatcher = /(^.*?[.!?])(?:\s|$)(.*)|(.*)/s;
-const tagMatcher = new RegExp(`(?<=\\s|^)${tagCharacter}\\w+\\b`, 'g');
+const tagMatcher = new RegExp(`(?<=\\s|^)${tagCharacter}(\\w+)\\b`, 'g');
 
 const tagToLowercase = tag => tag.toLowerCase();
 const removeDuplicateTags = (unique, item) => (unique.includes(item) ? unique : [...unique, item]);
+
+const colorTags = (str) => str.replace(tagMatcher, chalk.blue('$1'));
 
 class Entry {
   constructor(entryText) {
@@ -14,8 +18,14 @@ class Entry {
     this.text = entry[2] ? entry[2] : '';
     this.tags = (entryText.match(tagMatcher) || [])
       .map(tagToLowercase)
-      .reduce(removeDuplicateTags, []);
+      .reduce(removeDuplicateTags, [])
+      .sort();
     this.timeStamp = Date.now();
+  }
+
+  toString() {
+    const timeStamp = new Date(this.timeStamp);
+    return `${chalk.blue(timeStamp.toLocaleString())} - ${colorTags(this.title)}\n\n${colorTags(this.text)}`;
   }
 }
 
