92d0de1befe1df299137765f2c99ebfe6373686a
bothzoli <bothzoli@gmail.com>
2020-01-28 20:36:39 +0100

    Fix util tests

--- src/util/util.js
+++ src/util/util.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 
 module.exports = async (data) => {
   const entries = (await fs.promises.readFile('./entries.json', 'utf8').catch(() => null)) || '{"Entries":[]}';
+
   const jsonEntries = JSON.parse(entries);
   jsonEntries.Entries.push(data);
-  console.log(jsonEntries);
+
   await fs.promises.writeFile('./entries.json', JSON.stringify(jsonEntries));
 };
--- src/util/util.test.js
+++ src/util/util.test.js
@@ -1,18 +1,24 @@
-// const fs = require('fs');
-// const util = require('./util');
+const fs = require('fs');
+const util = require('./util');
 
-// test('Add 1 entry to non-existing entries', async () => {
-//   fs.promises.readFile = jest.fn(() => Promise.reject());
-//   expect.assertions(2);
-//   const exists = await util('asdf');
-//   expect(exists.Entries.length).toBe(1);
-//   expect(exists.Entries[0]).toBe('asdf');
-// });
+test('Add 1 entry to non-existing entries', async () => {
+  fs.promises.readFile = jest.fn(() => Promise.reject());
+  fs.promises.writeFile = jest.fn();
 
-// test('Add 1 entry to already existing entries', async () => {
-//   fs.promises.readFile = jest.fn(() => Promise.resolve('{"Entries":["qwer"]}'));
-//   expect.assertions(2);
-//   const exists = await util('asdf');
-//   expect(exists.Entries.length).toBe(2);
-//   expect(exists.Entries[1]).toBe('asdf');
-// });
+  await util('asdf');
+
+  const writeFileArgument = fs.promises.writeFile.mock.calls[0][1];
+
+  expect(writeFileArgument).toBe('{"Entries":["asdf"]}');
+});
+
+test('Add 1 entry to already existing entries', async () => {
+  fs.promises.readFile = jest.fn(() => Promise.resolve('{"Entries":["qwer"]}'));
+  fs.promises.writeFile = jest.fn();
+
+  await util('asdf');
+
+  const writeFileArgument = fs.promises.writeFile.mock.calls[0][1];
+
+  expect(writeFileArgument).toBe('{"Entries":["qwer","asdf"]}');
+});
