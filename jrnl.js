ad1cb0d1c5635dac304239896ea46f8d97848b2f
bothzoli <bothzoli@gmail.com>
2020-02-11 20:31:30 +0100

    Add yargs for list functionality

--- jrnl.js
+++ jrnl.js
@@ -1,30 +1,36 @@
+#!/usr/bin/env node
+
 const readline = require('readline');
-const { Entry, entryToString, listEntries } = require('./src/entry');
+const yargs = require('yargs');
+const { Entry, listEntries } = require('./src/entry');
 const { addNewEntry } = require('./src/util/util');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: '',
-});
+const { argv } = yargs;
 
-listEntries(console.log, 3);
-
-rl.write('Please write your jrnl entry!\n');
-rl.prompt();
-
-let jrnlEntry = '';
-
-(async () => {
-  rl.on('line', (line) => {
-    jrnlEntry += `${line}\n`;
-    rl.prompt();
-  }).on('close', async () => {
-    const newEntry = Entry(jrnlEntry);
-
-    entryToString(newEntry);
-    await addNewEntry(newEntry);
-
-    listEntries(console.log, 3);
+if (argv.l >= 0) {
+  listEntries(console.log, argv.l);
+} else if (argv._.length === 0) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '',
   });
-})();
+
+  rl.write('Please write your jrnl entry!\n');
+  rl.prompt();
+
+  let jrnlEntry = '';
+
+  (async () => {
+    rl.on('line', (line) => {
+      jrnlEntry += `${line}\n`;
+      rl.prompt();
+    }).on('close', async () => {
+      const newEntry = Entry(jrnlEntry);
+
+      await addNewEntry(newEntry);
+
+      listEntries(console.log, 1);
+    });
+  })();
+}
