8a27a2070bcc30dff3250d22b438228f8afb49ec
bothzoli <bothzoli@gmail.com>
2020-03-23 21:00:28 +0100

    Change entries path to home dir

--- settings.js
+++ settings.js
@@ -1,7 +1,13 @@
+const os = require('os');
+const path = require('path');
+
+const homeDir = os.homedir();
+const entriesPath = path.join(homeDir, 'jrnl.json');
+
 module.exports = {
   tagCharacter: '~',
   timeStampFormat: 'YYYY-MM-DD HH:mm',
   tagColor: 'green',
   timeStampColor: 'green',
-  entriesPath: './entries.json'
+  entriesPath
 };
